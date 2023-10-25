70a31e6c743e6395f0a89392b4718fd6fdab9868
shyam <113759633+003-kalki@users.noreply.github.com>
2023-10-25 20:10:45 +0530

    Update blockchain101.js
    first update

--- blockchain101.js
+++ blockchain101.js
@@ -1,31 +1,47 @@
-//color change of pasage class//
+
 const passageElements = document.querySelectorAll(".button");
 
-passageElements.forEach(passage => {
-    passage.addEventListener("mouseenter", function (event) {
-        passage.style.color = "white";
-    });
+passageElements.forEach((passage) => {
+  passage.addEventListener("mouseenter", function (event) {
+    passage.style.color = "white";
+  });
 
-    passage.addEventListener("mouseleave", function (event) {
-        passage.style.color = "#E48D0A";
-    });
+  passage.addEventListener("mouseleave", function (event) {
+    passage.style.color = "#E48D0A";
+  });
 });
 
-const button = document.getElementById("button1"); // Remove the # symbol
-button.addEventListener("click", function() {
-    // Replace the URL with the path to your reference.html file
-    const referenceMaterialURL = "reference.html";
-    
-    // Open a new window or tab with the reference.html file
-    window.open(referenceMaterialURL, "_blank");
+const button = document.getElementById("button1");
+button.addEventListener("click", function () {
+  
+  const referenceMaterialURL = "reference.html";
+
+  // Open a new window or tab with the reference.html file
+  window.open(referenceMaterialURL, "_blank");
 });
 
+const button2 = document.getElementById("button2"); 
+button2.addEventListener("click", function () {symbolsymbol
+ 
+  const referenceMaterialURL = "basic.html";
 
+ 
+                                               
+  window.open(referenceMaterialURL, "_blank");
+});
 
-
-
-
-
-
-
-
+var btc = document.getElementById("bitcoin");
+var eth = document.getElementById("ethereum");
+var doge = document.getElementById("dogecoin");
+var settings = {
+  async: true,
+  crossDomain: true,
+  url: "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd", // Use "usd" instead of "inr"
+  method: "GET",
+  headers: {},
+};
+$.ajax(settings).done(function (response) {
+  btc.innerHTML = response.bitcoin.usd;
+  eth.innerHTML = response.ethereum.usd;
+  doge.innerHTML = response.dogecoin.usd;
+});
