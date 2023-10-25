7ae3110cca8ccf75768dfa4cabe28a2c83f167c4
shyam <113759633+003-kalki@users.noreply.github.com>
2023-10-25 20:11:14 +0530

    Update blockchain101.js

--- blockchain101.js
+++ blockchain101.js
@@ -36,7 +36,7 @@ var doge = document.getElementById("dogecoin");
 var settings = {
   async: true,
   crossDomain: true,
-  url: "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd", // Use "usd" instead of "inr"
+  url: "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
   method: "GET",
   headers: {},
 };
